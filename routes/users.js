d9f6e0b55def1d66b035b7e247a06874de9c1ffe
PavloVeretilnyi <pashtet9112@gmail.com>
2025-08-22 07:44:56 -0400

    Secind commit

--- routes/users.js
+++ routes/users.js
@@ -38,6 +38,33 @@ router.get("/:email",(req,res)=>{
   res.send(filtered_users);
 });
 
+// GET by lastName request: Retrieve a single user with lastName
+router.get("/lastName/:lastName", (req, res) => {
+    // Extract the lastName parameter from the request URL
+    const lastName = req.params.lastName;
+    // Filter the users array to find users whose lastName matches the extracted lastName parameter
+    let filtered_lastname = users.filter((user) => user.lastName === lastName);
+    // Send the filtered_lastname array as the response to the client
+    res.send(filtered_lastname);
+});
+
+// Function to convert a date string in the format "dd-mm-yyyy" to a Date object
+function getDateFromString(strDate) {
+    let [dd, mm, yyyy] = strDate.split('-');
+    return new Date(Number(yyyy), Number(mm) - 1, Number(dd)); 
+}
+
+// Define a route handler for GET requests to the "/sort" endpoint
+router.get("/sort", (req, res) => {
+    // Sort the users array by DOB in ascending order
+    let sorted_users = users.sort(function(a, b) {
+        let d1 = getDateFromString(a.DOB);
+        let d2 = getDateFromString(b.DOB);
+        return d1 - d2;
+    });
+    // Send the sorted_users array as the response to the client
+    res.send(sorted_users);
+});
 
 // POST request: Create a new user
 router.post("/",(req,res)=>{
